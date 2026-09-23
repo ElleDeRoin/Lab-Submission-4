c5e18705f7d315c10e3ab9a7a77ebb6bf3872bc4
Cami Alarcon-Fernandez <camialarconfern@gmail.com>
2026-09-23 19:34:56 -0400

    Revert Meteor and Big Meteor scripts to original state

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -5,20 +5,15 @@ using UnityEngine;
 public class Meteor : MonoBehaviour
 {
 
-    private void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
-        //transform.Translate(Vector3.down * Time.deltaTime * 2f);
+        transform.Translate(Vector3.down * Time.deltaTime * 2f);
 
-        //if (transform.position.y < -11f)
-        //{
-        //    Destroy(this.gameObject);
-        //}
+        if (transform.position.y < -11f)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D whatIHit)
