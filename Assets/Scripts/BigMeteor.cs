2f4cddb343d266ad316bf8e63cfec81fb58f04dc
ElleDeRoin <107569262+ElleDeRoin@users.noreply.github.com>
2026-09-23 19:00:44 -0400

    fix

--- Assets/Scripts/BigMeteor.cs
+++ Assets/Scripts/BigMeteor.cs
@@ -6,8 +6,6 @@ using Cinemachine;
 public class BigMeteor : MonoBehaviour
 {
     private int hitCount = 0;
-    private int normalSize = 60;
-    private int bigSize = 90;
 
     // Start is called before the first frame update
     void Start()
